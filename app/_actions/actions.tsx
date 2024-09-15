c806315b65d58ee4362b799c5abdea5a5f9e0849
blackie360 <felixkent360@gmail.com>
2024-09-15 14:31:05 +0300

    user can add comment

--- app/_actions/actions.tsx
+++ app/_actions/actions.tsx
@@ -3,7 +3,33 @@
 import { redirect } from "next/navigation";
 import { cookieBasedClient } from "../utils/amplify-utils";
 import { revalidatePath } from "next/cache";
+import { Schema } from "@/amplify/data/resource";
 
+
+export async function deleteComment(formData: FormData) {
+  const id = formData.get("id")?.toString();
+  if (!id) return;
+  const { data: deletedComment } =
+    await cookieBasedClient.models.Comment.delete({
+      id,
+    });
+  console.log("deleted", deletedComment);
+}
+
+export async function addComment(
+  content: string,
+  post: Schema["Post"]["type"],
+  paramsId: string
+) {
+  if (content.trim().length === 0) return;
+  const { data: comment } = await cookieBasedClient.models.Comment.create({
+    postId: post.id,
+    content,
+  });
+
+  console.log("got comment", comment);
+  revalidatePath(`/post/${paramsId}`);
+}
 
 export async function onDeletePost(id: string) {
   const { data, errors } = await cookieBasedClient.models.Post.delete({
