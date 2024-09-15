f54366de73ba65f64aecacb9abdd265c3e451aae
blackie360 <felixkent360@gmail.com>
2024-09-15 11:52:38 +0300

    feat:account creation and sign in & out

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Auth from "@/components/auth/Auth";
+import NavBar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      ><Auth>
+      > <NavBar />
+        <Auth>
         {children}
       </Auth>
         
